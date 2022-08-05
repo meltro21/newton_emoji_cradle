920d74fbaf39dd9e5ef0544ce45839d3aeb237b6
meltro <meltro212121@gmail.com>
2022-08-05 23:51:37 +0500

    Smiley face Done

--- lib/main.dart
+++ lib/main.dart
@@ -44,17 +44,18 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SafeArea(
         child: Center(
           child: Container(
-            height: height * 0.6,
-            width: width * 0.6,
-            color: Colors.red,
+            height: height * 0.7,
+            width: width * 0.7,
+            //color: Colors.red,
             child: Stack(
               children: [
                 // Bar
                 Container(
-                  margin: EdgeInsets.only(left: height * 0.04),
-                  color: Colors.blue,
-                  height: height * 0.6,
-                  width: height * 0.6,
+                  margin:
+                      EdgeInsets.only(left: width * 0.04, right: width * 0.04),
+                  //color: Colors.blue,
+                  height: height * 0.7,
+                  width: width * 0.7,
                   child: CustomPaint(
                     size: Size.zero,
                     painter: Bar(barHeight, barWidth),
@@ -67,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   left: 0,
                   child: Container(
                     height: barHeight * 0.1,
-                    width: (((height * 0.04) * 2) + barHeight * 1),
+                    width: (((width * 0.04) * 2) + barWidth * 1),
                     decoration: const BoxDecoration(
                       color: Color(0xff071013),
                       borderRadius: BorderRadius.only(
@@ -78,13 +79,28 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                 //Smily Face
+                //line
                 Positioned(
-                  left: width * 0.2,
+                  left: barWidth * 0.3,
                   top: barHeight * 0.012,
                   child: Container(
-                    width: 1,
+                    width: barWidth * 0.003,
                     height: barHeight * 0.6,
                     decoration: BoxDecoration(color: Color(0xffAAAAAA)),
+                  ),
+                ),
+                //face
+                Positioned(
+                  left: barWidth * 0.3 - (barHeight * 0.15 / 2),
+                  top: barHeight * 0.6,
+                  child: Container(
+                    height: barHeight * 0.15,
+                    width: barHeight * 0.15,
+                    child: smilyFace(barHeight * 0.15),
+                    decoration: BoxDecoration(
+                      color: Color(0xffeDBF37),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
                   ),
                 )
               ],
@@ -96,21 +112,102 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+Widget smilyFace(double size) {
+  return Stack(
+    children: [
+      //leftEye
+      Positioned(
+        top: size * 0.3,
+        left: size * 0.2,
+        child: Transform.rotate(
+          angle: pi / 4,
+          child: Container(
+            height: size * 0.05,
+            width: size * 0.2,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      Positioned(
+        top: size * 0.3 + size * 0.09,
+        left: size * 0.25,
+        child: Transform.rotate(
+          angle: -pi / 12,
+          child: Container(
+            height: size * 0.05,
+            width: size * 0.15,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      //rightEye
+      Positioned(
+        top: size * 0.3,
+        left: size * 0.6,
+        child: Transform.rotate(
+          angle: -pi / 4,
+          child: Container(
+            height: size * 0.05,
+            width: size * 0.2,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      Positioned(
+        top: size * 0.3 + size * 0.09,
+        left: size * 0.6,
+        child: Transform.rotate(
+          angle: pi / 12,
+          child: Container(
+            height: size * 0.05,
+            width: size * 0.15,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      Container(
+          margin: EdgeInsets.only(top: size * 0.01),
+          child: CustomPaint(painter: SmilyFaceSmile(), size: Size(size, size)))
+    ],
+  );
+}
+
+class SmilyFaceSmile extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()..color = Colors.black;
+    canvas.drawArc(
+      Rect.fromCenter(
+        center: Offset(size.width / 2, size.height / 2 + (size.height * 0.15)),
+        height: size.height * 0.6,
+        width: size.width * 0.6,
+      ),
+      -pi,
+      -pi,
+      false,
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
+
 class Bar extends CustomPainter {
-  double mysize;
+  double myHeight;
   double myWidth;
-  Bar(this.mysize, this.myWidth);
+  Bar(this.myHeight, this.myWidth);
   @override
   void paint(Canvas canvas, Size size) {
     print('size is $size');
     //leftBar
     var leftBar = Rect.fromPoints(
-        Offset(0, mysize * 0.14), Offset(mysize * 0.025, mysize * 0.9));
+        Offset(0, myHeight * 0.14), Offset(myWidth * 0.025, myHeight * 0.9));
 
     canvas.drawRect(leftBar, Paint()..color = Color(0xffAAAAAA));
 //leftArc
     final arcRect = Rect.fromLTRB(
-        mysize * 0.012, mysize * 0.012, mysize * 0.3, mysize * 0.3);
+        myWidth * 0.012, myHeight * 0.012, myWidth * 0.3, myHeight * 0.3);
     final startAngle1 = -pi / 2;
     final sweepAngle1 = -pi / 2;
     canvas.drawArc(
@@ -120,16 +217,16 @@ class Bar extends CustomPainter {
         false,
         Paint()
           ..color = Color(0xffAAAAAA)
-          ..strokeWidth = mysize * 0.025
+          ..strokeWidth = myWidth * 0.025
           ..style = PaintingStyle.stroke);
     //topBar
-    var topBarCaenter = Offset(mysize / 2, mysize * 0.0125);
+    var topBarCaenter = Offset(myWidth / 2, myHeight * 0.0125);
     var topBarRect = Rect.fromCenter(
-        center: topBarCaenter, width: (mysize * 0.7), height: mysize * 0.025);
+        center: topBarCaenter, width: (myWidth * 0.7), height: myWidth * 0.025);
     canvas.drawRect(topBarRect, Paint()..color = Color(0xffAAAAAA));
     //right arc
-    final arcRect2 = Rect.fromLTRB((mysize * 0.7), mysize * 0.012,
-        (mysize * 1) - mysize * 0.012, mysize * 0.3);
+    final arcRect2 = Rect.fromLTRB((myWidth * 0.7), myHeight * 0.012,
+        (myWidth * 1) - myWidth * 0.012, myHeight * 0.3);
     final startAngle2 = -pi / 2;
     final sweepAngle2 = pi / 2;
     canvas.drawArc(
@@ -139,11 +236,11 @@ class Bar extends CustomPainter {
         false,
         Paint()
           ..color = Color(0xffAAAAAA)
-          ..strokeWidth = mysize * 0.025
+          ..strokeWidth = myWidth * 0.025
           ..style = PaintingStyle.stroke);
     //rightBar
-    var rightBar = Rect.fromPoints(Offset(mysize * 0.975, mysize * 0.14),
-        Offset(mysize * 1, mysize * 0.9));
+    var rightBar = Rect.fromPoints(Offset(myWidth * 0.975, myHeight * 0.14),
+        Offset(myWidth * 1, myHeight * 0.9));
 
     canvas.drawRect(rightBar, Paint()..color = Color(0xffAAAAAA));
   }
